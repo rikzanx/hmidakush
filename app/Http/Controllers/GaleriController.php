7aebc05f41107dfdea0c56465b7880ce89e98b6e
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2024-02-16 15:50:53 +0700

    ad galeri paginate to 9

--- app/Http/Controllers/GaleriController.php
+++ app/Http/Controllers/GaleriController.php
@@ -11,11 +11,11 @@ class GaleriController extends Controller
     public function index(Request $request)
     {
         $kategori_list = KategoriGaleri::get();
-        $galeri_list = Galeri::with("kategori")->paginate(3);
+        $galeri_list = Galeri::with("kategori")->paginate(9);
         $kategori = "Semua";
         if ($request->has('kategori')) {
             $kategori_opt = KategoriGaleri::where('nama_kategori', $request->kategori)->firstOrFail();
-            $galeri_list = Galeri::where("kategori_galeri_id", $kategori_opt->id)->paginate(3);
+            $galeri_list = Galeri::where("kategori_galeri_id", $kategori_opt->id)->paginate(9);
             $kategori = $request->kategori;
         }
         // dd($galeri_list);
