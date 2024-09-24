2702965cacb05cb9e50ca7663cf6048dce01456e
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2024-09-24 19:25:32 +0700

    Update struktur-kepengurusan.blade.php

--- resources/views/frontend/struktur-kepengurusan.blade.php
+++ resources/views/frontend/struktur-kepengurusan.blade.php
@@ -14,5 +14,5 @@
   </div>
   <!-- Header End -->
 
-  <img style= "width:100%;" class="foto-struktural" src="{{ asset('img/struktur-kepengurursan-2023-2024.jpg') }}">
+  <img style= "width:100%;" class="foto-struktural" src="{{ asset('img/struktur-kepengurusan-2024-2025.png') }}">
 @endsection
