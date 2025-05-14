c1dd140f8e8d3fe6e1d97103d864a94639688c19
Muhammad Rikzan <30933800+rikzanx@users.noreply.github.com>
2025-05-14 11:39:24 +0700

    update sambutan

---
+++ app/Sambutans.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sambutan extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'id';
+    protected $fillable =
+    [
+        'nama',
+        'jabatan',
+        'isi'
+    ];
+}
---
+++ database/migrations/2025_05_14_113300_create_sambutans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSambutansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sambutans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->longText('isi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sambutans');
+    }
+}
